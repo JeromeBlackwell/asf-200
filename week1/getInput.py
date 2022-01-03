f5e940d4a71487fddba35bbd01ee7bb5c06ee297
JeromeBlackwell <71746521+JeromeBlackwell@users.noreply.github.com>
2022-01-03 11:52:48 -0500

    week1

--- week1/getInput.py
+++ week1/getInput.py
@@ -1,2 +1,8 @@
-cityZipcode = input("What is your hometown city: " + "What is your zipcode:")
-cityZipcode = int(cityZipcode)
+# cityZipcode = input("What is your hometown city: " + "What is your zipcode:")
+# cityZipcode = int(cityZipcode)
+
+
+city = input("What is your hometown city: ")
+zipcode = input("What is your zipcode: ")
+together = city + zipcode
+print (together)
