340926be441980de84b29529bbad16f53e1bd54d
JeromeBlackwell <71746521+JeromeBlackwell@users.noreply.github.com>
2022-01-08 02:54:20 -0500

    week2

---
+++ week2/class.py
@@ -0,0 +1,12 @@
+# class myClass():
+#     def method1(name = input ("Enter your name: ")
+#     print ("name.upper()"))
+#     def method2(BirthPlace = input ("Enter your BirthPlace"))
+#     print ("BirthPlace".upper())
+
+#     def main()
+
+name = input ("Enter your name: ")
+birthPlace = input("Enter you Place of BirthPlace: ")
+print (name.upper())
+print (birthPlace.upper())
