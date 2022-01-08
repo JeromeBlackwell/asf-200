948a08992cbc4f564b23302280f0c0b404811fb3
JeromeBlackwell <71746521+JeromeBlackwell@users.noreply.github.com>
2022-01-08 01:12:35 -0500

    week2

--- week1/getInput.py
+++ week1/getInput.py
@@ -1,8 +1,3 @@
-# cityZipcode = input("What is your hometown city: " + "What is your zipcode:")
-# cityZipcode = int(cityZipcode)
-
-
 city = input("What is your hometown city: ")
 zipcode = input("What is your zipcode: ")
 together = city + zipcode
-print (together)
---
+++ week2/variable.py
@@ -0,0 +1,15 @@
+a = "Your son John is failing "
+b = 2
+c = " subjects."
+print(a+str(b)+c)
+
+d = "John will need to redo "
+e = 2
+f = " courses."
+print(d+str(e)+f)
+
+g = "Eric is doing"
+h = "well in geograph"
+print (g+h)
+
+
