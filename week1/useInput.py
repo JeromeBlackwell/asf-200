6445cf9caca861a6fba01b7202071031b36b510a
JeromeBlackwell <71746521+JeromeBlackwell@users.noreply.github.com>
2022-01-10 13:04:38 -0500

    week1

--- week1/useInput.py
+++ week1/useInput.py
@@ -1,3 +1,4 @@
 name =input("What is your name? ")
 age =input("What is your age? ")
-print("Hi " + name, "as of this year " + age )
+year =(100 - int(age))+ 2021
+print("Hi " + name +" as of this year you are " + age + " You will turn 100 in the " + str(year) + "." )
--- week2/class.py
+++ week2/class.py
@@ -1,23 +1,24 @@
-name = input ("Enter your name: ")
-birthPlace = input("Enter you Place of BirthPlace: ")
-print (name.upper())
-print (birthPlace.upper())
+# name = input ("Enter your name: ")
+# birthPlace = input("Enter you Place of BirthPlace: ")
+# print (name.upper())
+# print (birthPlace.upper())
 
-# class myClass():
-#     def _init_(self, first_name, last_name, place_of_birth):
-#         self.first_name = first_name
-#         self.last_name = last_name
-#         self.place_of_birth = place_of_birth
 
-#     def _str_(self):
-#         s = ['-----',
-#             f"First Name : {self.first_name}",
-#             f"Last_name : {self.last_name}",
-#             f"Place of Birth : {self.place_of_birth}"]
-#         return "\n".joins.upper(s)
+class myClass:
+    def __init__(self, first_name, last_name, place_of_birth):
+        self.first_name = first_name
+        self.last_name = last_name
+        self.place_of_birth = place_of_birth
 
-#     print ("name.upper()"))
-#     def method2(BirthPlace = input ("Enter your BirthPlace"))
-#     print ("BirthPlace".upper())
+    def _str_(self):
+        s = f"First Name : {self.first_name.upper()}, Last_name : {self.last_name.upper()}, Place of Birth : {self.place_of_birth.upper()}"
+        return (s)
 
-#     def main()
+person = myClass(input ("Enter your First Name: "), input ("Enter your Last Name: "),  input("Enter you Place of BirthPlace: "))
+print(person._str_())
+
+    # print ("name.upper()")
+    # def method2(BirthPlace = input ("Enter your BirthPlace"))
+    # print ("BirthPlace".upper())
+
+    # def main()
